2c2bc32cca1e1073aa6206fc829c88805c213fbf
Bhubon Sutradhar Nil <sdbhubon@gmail.com>
2023-08-20 18:09:52 +0600

    Complete module 8 assignemnt with emit

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps(['message'])
+const emits = defineEmits('update:message')
+
+function messageEmitted() {
+    emits('update:message', 'Hello World From Child Emit')
+}
+
+</script>
+
+<template>
+    <div>
+        <p class="mt-8">Child Component</p>
+        <h2 class="text-xl my-3">Message: <span class="text-green-600">{{ message }}</span></h2>
+        <button @click="messageEmitted()"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold my-8 py-2 px-4 rounded">Change Message</button>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref, reactive } from 'vue';
+import ChildComponent from './ChildComponent.vue';
+const message = ref('Hello World')
+</script>
+
+<template>
+    <p>Parent Component</p>
+    <p class="my-5 text-xl">Message: <span class="text-green-600">{{ message }}</span></p>
+    <hr>
+    <ChildComponent v-model:message="message" />
+</template>
+
+<style scoped></style>
